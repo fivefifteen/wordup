a862ff3d8de5165219766b2967922b62e8947927
Kodie Grantham <kodie.grantham@gmail.com>
2024-12-05 13:27:00 -0600

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# WordUp
+
+An advanced WordPress [Deployer Recipe](https://deployer.org).
+
+Very much in a pre-alpha stage. You probably shouldn't use this (at least not yet).
+
+```
+// @TODO: Write documentation
+```
+
+## License Information
+
+MIT. See the [license.md file](license.md) for more info.
---
+++ recipes/wordup.php
@@ -0,0 +1,22 @@
+<?php
+use function \Deployer\{
+  add,
+  import,
+  set
+};
+
+require_once('recipe/common.php');
+
+add('recipes', array('wordup'));
+
+set('deploy_path', '/var/www/public_html');
+set('public_path', '/var/www/public');
+set('db_backups_path', 'db_backups');
+set('wp_content_dir', 'wp-content');
+set('wp_uploads_dir', '{{wp_content_dir}}/uploads');
+set('shared_files', array('wp-config.php'));
+set('shared_dirs', array('{{wp_uploads_dir}}'));
+set('writable_dirs', array('{{wp_uploads_dir}}'));
+
+import('config.yml');
+?>
