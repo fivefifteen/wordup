9fb20298b9d78dac50180b9f86a66bea6de4532d
Kodie Grantham <kodie.grantham@gmail.com>
2024-12-06 10:34:21 -0600

    Rename WordUp class to Recipe and make it part of the WordUp namespace

--- src/Recipe.php
+++ src/Recipe.php
@@ -1,5 +1,7 @@
 <?php
-class WordUp {
+namespace WordUp;
+
+class Recipe {
   function __construct($config_file = null) {
     global $config_file;
     require_once(__DIR__ . '/../recipe/wordup.php');
